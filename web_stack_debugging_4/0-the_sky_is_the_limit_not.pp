ea7fe8edd1eed3c4e33378e164476be3d8fb95b6
SugiraVinc <sugiravincent11@gmail.com>
2022-12-09 17:09:29 +0000

    fix

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,8 +5,8 @@ service { 'nginx':
 
 exec { 'increase ULIMIT':
   # changing user limit
-  command =>  'sed -i "s/ULIMIT=\"-n 15\"-n 4000\"/g" /etc/default/nginx',
+  command  =>  'sed -i "s/ULIMIT=\"-n 15\"-n 4000\"/g" /etc/default/nginx',
   provider => 'shell',
   # nginx restart
-  notify => Service['nginx']
+  notify   => Service['nginx']
 }
