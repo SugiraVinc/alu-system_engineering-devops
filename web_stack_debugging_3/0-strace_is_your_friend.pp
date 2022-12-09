e559f6bd9aa97cd41939073b2446639bbba0e076
SugiraVinc <sugiravincent11@gmail.com>
2022-12-09 08:23:21 -0800

    more

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Puppet fixing
 exec { 'config filename fix':
   command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-  provide => 'shell'
+  provider => 'shell'
 }
